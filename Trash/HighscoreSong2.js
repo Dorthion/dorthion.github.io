fdf34251d181a5f5a6afd60e8f4d479ff97c5d4d
Dorthion <beleg.dorthion@gmail.com>
2018-06-05 13:08:50 +0200

    Summoning circles with 50px/50px
    4321....

--- Trash/HighscoreSong2.js
+++ Trash/HighscoreSong2.js
@@ -1,18 +1,19 @@
 //Wczytanie stylu kółek
-let licznik3=0;
-		var n1 = new Image ('MusicIcons/1.jpg');
-		var n2 = new Image ('MusicIcons/2.jpg');
-		var n3 = new Image ('MusicIcons/3.jpg');
-		var n4 = new Image ('MusicIcons/4.jpg');
-		console.log(song[2+licznik3]);
+		let licznik3=0;
+		var gra = document.createElement('div');
+		gra.setAttribute("id","gra");
+		document.getElementById('MainMenu').appendChild(gra);
 		var startTime3 = Date.now();
 		var interval3 = setInterval(function() {
 			var elapsedTime3 = Date.now() - startTime3;
 			if(parseInt(song[2+licznik3])<elapsedTime3)
 			{
-				console.log(parseInt(song[2+licznik3]));
+				console.log(song[2+licznik3]);
 				console.log(elapsedTime3);
 				console.log("woot");
+				let reszta=licznik3%4;
+				let Jppg = 'n'+reszta;
+				gra.innerHTML += '<img src="MusicIcons/'+(reszta+1)+'.png" />';
 				licznik3=licznik3+3;
 			}
 }, 1);
